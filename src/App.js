5f5bb905e1424b9d6f5d7a27ab5f512fc6958fa2
Vansh Garg <vanshgarg0706@gmail.com>
2023-09-01 14:16:18 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import people from "./people";
+import React, { useEffect, useState } from 'react';
+import "./App.css";
+let counter=0;
 
 function App() {
+  const [data,setdata] = useState(people);
+
+  const [count,setCount] = useState(-1);
+
+  
+
+  useEffect(()=>{
+    setCount(count+1)
+  },[data]);
+
+
+  const removeItem = (data,individual)=>{
+      setdata([...data].filter((item)=>{return item !== individual}));
+      
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main">
+        <h1>Birthdays</h1>
+        {data.map((person) => {
+          return (
+            <>
+              <div className="container">
+                <div className="image">
+                  <img src={person.image} alt="person" />
+                </div>
+                <div className="personal">
+                  <p>{person.name}</p>
+                  <p>{person.age} years</p>
+                </div>
+                <div className="birthday">
+                  <p>Birthday: {person.birthdate}</p>
+                </div>
+                <div className="btn-remove">
+                  <button type="button" onClick={()=>removeItem(data,person)}>
+                    <img src="./images/RemoveBOx.png" alt="remove" />
+                  </button>
+                </div>
+              </div>
+            </>
+          );
+        })}
+        <button type="button" className="remove-all" onClick={()=>setdata([])}>Clear All</button>
+      </div>
+      <p className="person-remove">People Removed: {count}</p>
+    </>
   );
 }
 
---
+++ src/people.js
@@ -0,0 +1,44 @@
+const birthday = [{
+    name: 'Akshita Garg',
+    age: 24,
+    birthdate: '14 October',
+    image: './images/kelly-sikkema-JN0SUcTOig0-unsplash (1).jpg'
+},
+{
+    name: 'Sanjay Garg',
+    age: 58,
+    birthdate: '30 December',
+    image: './images/vicky-hladynets-C8Ta0gwPbQg-unsplash.jpg'
+},{
+    name: 'Meena',
+    age: 48,
+    birthdate: '12 November',
+    image: './images/stefan-stefancik-QXevDflbl8A-unsplash.jpg'
+},{
+    name: 'Sukhmeen',
+    age: 23,
+    birthdate: '22 August',
+    image : './images/michael-austin-jgSAuqMmJUE-unsplash.jpg'
+},{
+    name: 'Rita',
+    age: 97,
+    birthdate: '15 August',
+    image: './images/jake-nackos-IF9TK5Uy-KI-unsplash.jpg'
+},{
+    name: 'Vansh Garg',
+    age: 21,
+    birthdate: '06 March',
+    image: './images/ethan-hoover-0YHIlxeCuhg-unsplash.jpg'
+},{
+    name: 'Rahul Garg',
+    age: 33,
+    birthdate: '14 September',
+    image: './images/cap-2923682_1280.jpg'
+},{
+    name: 'Vanshika',
+    age: 78,
+    birthdate: '05 September',
+    image: './images/houcine-ncib-B4TjXnI0Y2c-unsplash.jpg'
+}];
+
+export default birthday;
